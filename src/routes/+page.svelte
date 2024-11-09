bae4f31e6d9e8f3baedf3992f0a5aeb6b7df8a02
oneezy <justinoneill2007@gmail.com>
2024-11-08 21:33:11 -0600

    added fullscreen button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,23 @@
 <script>
 	import Sensor from '$lib/sensor/Sensor.svelte';
+
+	const toggleFullscreen = async () => {
+		try {
+			if (!document.fullscreenElement) {
+				await document.documentElement.requestFullscreen();
+			} else {
+				await document.exitFullscreen();
+			}
+		} catch (error) {
+			console.error('Fullscreen failed', error);
+		}
+	};
 </script>
 
 <main class="flex h-screen flex-col items-center overflow-auto">
 	<h1 class="text-4xl font-bold">Sensors</h1>
 	<Sensor />
+	<button class="fixed bottom-4 right-4 bg-black text-white" onclick={toggleFullscreen}
+		>Toggle Fullscreen</button
+	>
 </main>
