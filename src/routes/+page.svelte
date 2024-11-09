caa03e49e15d1f0daecd0edbca2c3c148b15581d
oneezy <justinoneill2007@gmail.com>
2024-11-08 19:47:20 -0600

    added scrollbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import Sensor from '$lib/sensor/Sensor.svelte';
 </script>
 
-<main class="flex h-screen flex-col items-center justify-center">
+<main class="flex h-screen flex-col items-center overflow-auto">
 	<h1 class="text-4xl font-bold">Sensors</h1>
 	<Sensor />
 </main>
